07cbaa0c8b2fb80cc015a8e3b619c32f5a7bec2c
janelle.zorvan <janelle.zorvan@tn.gov>
2017-04-19 10:41:59 -0500

    deserializing Json and printing to console

--- People Data/Program.cs
+++ People Data/Program.cs
@@ -15,8 +15,30 @@ namespace People_Data
             string currentDirectory = Directory.GetCurrentDirectory();
             DirectoryInfo directory = new DirectoryInfo(currentDirectory);
             var fileName = Path.Combine(directory.FullName, "People.json");
+            var people = DeserializePeople(fileName);
+            foreach (var person in people)
+            {
+                Console.WriteLine("Name: " + person.FirstName + " " + person.LastName);
+                Console.WriteLine("Birthday: " + person.BirthDate.Date.ToString("d"));
+                Console.WriteLine("Address: " + person.StreetNumber + " " + person.StreetName);
+                Console.WriteLine("         " + person.City + ", " + person.State + " " + person.Zip);
+                Console.WriteLine("Phone: " + person.PhoneNumber + "  Email: " + person.Email);
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+
         }
 
-
+        public static List<Person> DeserializePeople (string fileName)
+        {
+            var people = new List<Person>();
+            var serializer = new JsonSerializer();
+            using (var reader = new StreamReader(fileName))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+                people = serializer.Deserialize< List<Person> > (jsonReader);
+            }
+                return people; 
+        }
     }
 }
