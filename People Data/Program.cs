1106f64676ee2fe3d5a5fdbf43ac22c66f378526
Janelle Zorvan <AG20375@tn.gov>
2017-04-19 09:50:10 -0500

    initial project

---
+++ People Data/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace People_Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string currentDirectory = Directory.GetCurrentDirectory();
+            DirectoryInfo directory = new DirectoryInfo(currentDirectory);
+            var fileName = Path.Combine(directory.FullName, "People.json");
+            var people = Des
+        }
+    }
+}
